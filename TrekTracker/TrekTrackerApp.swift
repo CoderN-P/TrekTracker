45a8221c8a2dc03f099ddd3e36edce0303ee5ff8
CoderN-P <neel.parpia@gmail.com>
2024-06-24 11:54:28 -0700

    Initial Commit

---
+++ TrekTracker/TrekTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekTrackerApp.swift
+//  TrekTracker
+//
+//  Created by Neel P on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TrekTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
